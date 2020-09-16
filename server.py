24d90a8135a27f82c7feee6b008a2f5242dc3527
Vorotov <64152947+Vorotov@users.noreply.github.com>
2020-09-16 17:03:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,73 @@
-# реализация сервера для тестирования метода get по заданию - Клиент для отправки метрик
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import asyncio
 
-import socket
 
-sock = socket.socket()
-sock.bind(('127.0.0.1', 10001))
-sock.listen(1)
-conn, addr = sock.accept()
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port
+    )
 
-print('Соединение установлено:', addr)
+    server = loop.run_until_complete(coro)
 
-# переменная response хранит строку возвращаемую сервером, если вам для
-# тестирования клиента необходим другой ответ, измените ее
-response = b'ok\npalm.cpu 10.5 1501864297\npalm.cpu 111.5 1501864277\neardrum.cpu 15.3 1501864259\n\n'
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    request = data.decode('utf-8')
-    print(f'Получен запрос: {ascii(request)}')
-    print(f'Отправлен ответ {ascii(response.decode("utf-8"))}')
-    conn.send(response)
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
-conn.close()
+
+class ClientServerProtocol(asyncio.Protocol):
+    big_data = {}
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        resp = self.process_data(data.decode())
+        self.transport.write(resp.encode())
+
+    @staticmethod
+    def process_data(data):
+        if data == "\n":
+            return "error\nwrong command\n\n"
+        data = data[0:len(data) - 1]
+        split_data = data.split()
+        if split_data[0] == "get":
+            if len(split_data) != 2:
+                return "error\nwrong command\n\n"
+            if split_data[1] == "*":
+                resp = "ok\n"
+                for k, el in ClientServerProtocol.big_data.items():
+                    for key, value in el.items():
+                        resp += "{0} {1} {2}\n".format(k, value, key)
+                resp += "\n"
+                return resp
+            elif split_data[1] in ClientServerProtocol.big_data:
+                resp = "ok\n"
+                for key, value in ClientServerProtocol.big_data[split_data[1]].items():
+                    resp += "{0} {1} {2}\n".format(split_data[1], value, key)
+                resp += "\n"
+                return resp
+            else:
+                return "ok\n\n"
+        elif split_data[0] == "put":
+            if len(split_data) != 4:
+                return "error\nwrong command\n\n"
+            try:
+                value = float(split_data[2])
+                timestamp = int(split_data[3])
+                if split_data[1] in ClientServerProtocol.big_data:
+                    ClientServerProtocol.big_data[split_data[1]][timestamp] = value
+                else:
+                    ClientServerProtocol.big_data[split_data[1]] = {timestamp: value}
+                return "ok\n\n"
+            except:
+                return "error\nwrong command\n\n"
+        else:
+            return "error\nwrong command\n\n"
+
+#run_server("127.0.0.1", 10001)
